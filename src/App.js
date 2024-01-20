b6797aa89fe8296b5039debbb63631f385ae61d5
Samruddhi Bhoyar <skbhoyar36.sb@gmail.com>
2024-01-21 00:34:47 +0530

    grid

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import Authentication from './components/Authentication/Authentication'
+import HomePage from './components/HomePage/HomePage'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className=''>
+      <Routes>
+        <Route
+          path='/'
+          element={true ? <HomePage /> : <Authentication />}
+        ></Route>
+      </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
